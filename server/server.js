03e3fc8177c743c508092afa2a99d60852090181
Badgers <brianbadger93@gmail.com>
2023-06-20 11:32:31 -0500

    fixed dist

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 
