e91577275e6b41f4bbe8d425d84b2706e615d939
Badgers <brianbadger93@gmail.com>
2023-06-20 11:05:59 -0500

    fixed database name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-recipes');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipes');
 
 module.exports = mongoose.connection;
